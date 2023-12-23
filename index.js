502969bf619360ba6d87c42afc24b09b2dc53398
swatiswagatika98 <swatiswagatika.1998mishra@gmail.com>
2023-12-23 08:13:04 -0800

    fixed bugs

---
+++ index.js
@@ -0,0 +1,7 @@
+const lis = document.getElementsByTagName("li");
+const mangoLi = lis[4];
+mangoLi.style.color = "blue";
+for(const Li of lis){
+    console.log(Li)
+    Li.style.fontStyle = "italic"
+}
---
+++ project-1/index.js
@@ -0,0 +1,8 @@
+// const mainHead = document.getElementById("main-heading");
+// console.log(mainHead)
+// mainHead.innerHTML="fruitworld";
+// mainHead.style.color="orange";
+// alert("Hello")
+console.log("Hello world");
+// document.getElementById("basket-heading").style.backgroundColor="red";
+document.getElementById("basket-heading").style.color="green";
